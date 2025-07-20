151d112a3750e52cca06953b2460bb5d2fd98e76
Shivam Sayankar <111188660+Shivam-Sayankar@users.noreply.github.com>
2025-07-20 19:44:39 +0530

    Add logic for a single round of the game

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     // const options = ["rock", "paper", "scissors"]
     // const randomIndex = Math.floor(Math.random() * 3); // 0 1 2
@@ -18,11 +21,45 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-// console.log(getComputerChoice())
 
 function getHumanChoice() {
     let humanChoice = prompt("Whats your choice? [rock, paper, scissors]: ")
-    console.log(humanChoice)
+    return humanChoice;
 }
 
-getHumanChoice()
+
+function playRound(humanChoice, computerChoice) {
+
+    humanChoice = humanChoice.toLowerCase();
+
+    // rock paper scissors
+
+    // rock vs scissors
+    // rock vs paper
+    // scissors vs paper
+
+    if ((humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        humanScore += 1;
+    } else if (humanChoice === computerChoice) {
+        console.log("You and the Computer chose the same thing!")
+    } else {
+        computerScore += 1;
+    }
+
+
+    if (humanScore > computerScore) {
+        console.log(`You Won! ${humanChoice} beats ${computerChoice}.`)
+    } else if (humanScore < computerScore) {
+        console.log(`You Lose! ${computerChoice} beats ${humanChoice}.`);
+    } else {
+        console.log(`It's a tie! Both, Computer and you chose ${humanChoice}.`);
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
